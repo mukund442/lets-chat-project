70719f64385af2d14af21de36bd17f6762bb88e3
mukund442 <92722963+mukund442@users.noreply.github.com>
2022-02-10 23:05:40 +0530

    Add files via upload

---
+++ letschat_page.js
@@ -0,0 +1,15 @@
+function send(){
+    msg=document.getElementById("msg").value;
+      firebase.database().ref(room_name).push({
+            name:user_name,
+            message:msg,
+            like:0
+      });
+      document.getElementById("msg").value=" ";
+}
+
+function logOut(){
+    localStorage.removeItem("user_name");
+    localStorage.removeItem("room_name");
+    window.location="index.html";
+}
